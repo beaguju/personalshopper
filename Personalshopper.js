a7ba3128e58b3a8357d04a9d97a6100c8c343647
Bea Gujuli <beatriz.gujuli@gmail.com>
2014-10-10 00:55:27 +0200

    Version 2: JS mousepointer01 + Nosotros

--- Personalshopper.js
+++ Personalshopper.js
@@ -1,3 +1,18 @@
-var photo = document.getElementById('image');
+var slide = document.getElementById('slide');
 
-photo.onmouseover
+slide.onmouseover = function() {
+	this.style.cursor="pointer";
+}
+
+var logo = document.getElementById('logo');
+
+logo.onmouseover = function() {
+	this.style.cursor="pointer";
+}
+
+var myli = document.getElementsByTagName('LI');
+
+myli.onmouseover = function() {
+	this.style.cursor="pointer";
+}
+
