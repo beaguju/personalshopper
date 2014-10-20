627bef2e7859d906548b4c194a38948756218b2e
marta <marta@example.com>
2014-10-20 14:38:41 +0200

    tercera version 20/10/2014

--- Personalshopper.js
+++ Personalshopper.js
@@ -1,18 +1,40 @@
-var slide = document.getElementById('slide');
-
-slide.onmouseover = function() {
-	this.style.cursor="pointer";
+//intervalo tiempo cambio img slide, 5 segundos, se llama a la función "avanzaSlide()"
+setInterval('avanzaSlide()',5000);
+ 
+//array con las clases para las diferentes imagenes
+var arrayImagenes = new Array(".img1",".img2",".img3");
+ 
+//variable que nos permitirá saber qué imagen se está mostrando
+var contador = 0;
+ 
+//hace un efecto fadeIn para mostrar una imagen
+function mostrar(img){
+	$(img).ready(function(){				
+  		$(arrayImagenes[contador]).fadeIn(1500);		
+	});
+}
+ 
+//hace un efecto fadeOut para ocultar una imagen
+function ocultar(img){
+	$(img).ready(function(){				
+  		$(arrayImagenes[contador]).fadeOut(1500);		
+	});
+}
+ 
+//función principal
+function avanzaSlide(){
+        //se oculta la imagen actual
+	ocultar(arrayImagenes[contador]);
+        //aumentamos el contador en una unidad
+	contador = (contador + 1) % 3;
+        //mostramos la nueva imagen
+	mostrar(arrayImagenes[contador]);
 }
 
-var logo = document.getElementById('logo');
+//HOME PAGE: Menú-Galería SERVICIOS
 
-logo.onmouseover = function() {
-	this.style.cursor="pointer";
+function mouseRollover(photo1) {
+	var old = photo1.src;
+	photo1.src = photo1.dataset.alt;
+	photo1.dataset.alt = old;
 }
-
-var myli = document.getElementsByTagName('LI');
-
-myli.onmouseover = function() {
-	this.style.cursor="pointer";
-}
-
