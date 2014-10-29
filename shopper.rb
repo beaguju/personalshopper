05721081af90555771f73cc2a33253291926276c
marta <marta@example.com>
2014-10-29 17:25:52 +0100

    version sinatra

---
+++ shopper.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'rubygems'
+
+get '/' do
+
+	erb :Personalshopper
+	
+end
+
+get '/Personalshopper' do
+	erb :PersonalShopper
+end
+
+get'/Nosotros' do
+	erb :Nosotros
+end
+
+get '/Servicios' do
+	erb :Servicios
+end
+	
+	get '/mujeres' do
+		erb :Mujeres
+	end
+
+	get '/hombres' do
+		erb :Hombres
+	end
+
+	get '/eventos' do
+		erb :Eventos
+	end
+
+	get '/empresas' do
+		erb :Empresas
+	end
+
+	get '/shopping' do
+		erb :shopping
+	end
+
+get '/Clientes' do
+	erb :Clientes
+end
+
+get '/Contacto' do
+	erb :Contacto
+end
